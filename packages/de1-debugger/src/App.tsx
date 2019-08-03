1e05ccbf92fd5283847755899179156d784810b2
Lukas Bombach <lukas.bombach@googlemail.com>
2019-08-03 09:49:36 +0200

    responsive design

--- packages/de1-debugger/src/App.tsx
+++ packages/de1-debugger/src/App.tsx
@@ -1,34 +1,36 @@
 import React, { useState } from "react";
-import { Layout, Row, Col } from "antd";
+import { Layout } from "antd";
 import ErrorBoundary from "./ErrorBoundary";
 import Connection from "./components/Connection";
 import Control from "./components/Control";
 import features from "./features";
+import "./App.css";
 
 const App: React.FC = () => {
   const [isConnected, setIsConnected] = useState(false);
 
+  const fullHeight = { height: "100vh" };
+
   return (
     <ErrorBoundary>
-      <Layout>
-        <Layout.Content>
-          <Row gutter={16}>
+      <Layout style={fullHeight}>
+        <Layout.Content style={fullHeight}>
+          <section className="app__grid" style={fullHeight}>
             {features
               .filter(f => !f.unused && !f.purposelyDisabled)
               .map(feature => (
-                <Col key={feature.feature} xs={24} sm={12} md={6} lg={8} xl={4}>
-                  <Control
-                    feature={feature.feature}
-                    uuid={feature.uuid}
-                    read={feature.read}
-                    write={feature.write}
-                    notify={feature.notify}
-                    connected={isConnected}
-                    values={feature.values}
-                  />
-                </Col>
+                <Control
+                  key={feature.feature}
+                  feature={feature.feature}
+                  uuid={feature.uuid}
+                  read={feature.read}
+                  write={feature.write}
+                  notify={feature.notify}
+                  connected={isConnected}
+                  values={feature.values}
+                />
               ))}
-          </Row>
+          </section>
           <Connection onChange={v => setIsConnected(v)} />
         </Layout.Content>
       </Layout>
