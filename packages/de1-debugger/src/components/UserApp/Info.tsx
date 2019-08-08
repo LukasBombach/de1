431b7999e3558e4e3cb89ef178c648977499b9b4
Lukas Bombach <lukas.bombach@googlemail.com>
2019-08-08 08:30:18 +0200

    displaying temperature

--- packages/de1-debugger/src/components/UserApp/Info.tsx
+++ packages/de1-debugger/src/components/UserApp/Info.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card } from "antd";
+import { Card, Descriptions } from "antd";
 import useNotify from "../../hooks/de1/useNotify";
 
 interface InfoProps {
@@ -7,15 +7,29 @@ interface InfoProps {
 }
 
 const Info: React.FC<InfoProps> = ({ isConnected }) => {
-  const [notify, , isNotifiying, stateInfos] = useNotify("stateInfo");
-  if (isConnected && !isNotifiying) notify();
+  const [notifyState, , notifyingState, stateInfos] = useNotify("stateInfo");
+  const [notifyShot, , notifyingShot, shotInfos] = useNotify("shot");
+
+  if (isConnected && !notifyingState) notifyState();
+  if (isConnected && !notifyingShot) notifyShot();
 
   const { state = null, substate = null } = stateInfos.reverse()[0] || {};
+  const { mixTemp = null, setMixTemp = null } = shotInfos.reverse()[0] || {};
 
   const stateText =
     state === "sleep" ? "sleeping" : substate ? substate : <em>loading...</em>;
+  const setMixTempText = setMixTemp ? `${setMixTemp}°C` : <em>loading...</em>;
+  const mixTempText = mixTemp ? `${mixTemp.toFixed(2)}°C` : <em>loading...</em>;
 
-  return <Card title="Info">{stateText}</Card>;
+  return (
+    <Card>
+      <Descriptions title="Info">
+        <Descriptions.Item label="State">{stateText}</Descriptions.Item>
+        <Descriptions.Item label="Goal">{setMixTempText}</Descriptions.Item>
+        <Descriptions.Item label="Temperature">{mixTempText}</Descriptions.Item>
+      </Descriptions>
+    </Card>
+  );
 };
 
 export default Info;
