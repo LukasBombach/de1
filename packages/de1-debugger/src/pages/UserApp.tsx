b776ed1d288f021a1b37acbe95a7f82e6d681a69
Lukas Bombach <lukas.bombach@googlemail.com>
2019-08-11 16:31:14 +0200

    experimental timeRemaining implementaion

--- packages/de1-debugger/src/pages/UserApp.tsx
+++ packages/de1-debugger/src/pages/UserApp.tsx
@@ -81,13 +81,17 @@ const MachineState: React.FC<{ isConnected: boolean }> = ({ isConnected }) => {
   );
 };
 
+function MMSS(millis: number) {
+  var minutes = Math.floor(millis / 60000);
+  var seconds = parseInt(((millis % 60000) / 1000).toFixed(0));
+  return minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
+}
+
 const Temperature: React.FC<{ isConnected: boolean }> = ({ isConnected }) => {
-  const [temperature, listenToTemperature] = useEvent("temperature");
   const [heatingInfo, listenHeating] = useEvent("heating");
   const [stateInfo, notifyAboutStates] = useNotify("stateInfo");
 
   if (isConnected) notifyAboutStates();
-  if (isConnected) listenToTemperature();
   if (isConnected) listenHeating();
 
   const isHeating = stateInfo && stateInfo.substate === "heating";
@@ -96,17 +100,23 @@ const Temperature: React.FC<{ isConnected: boolean }> = ({ isConnected }) => {
     <Card>
       <Statistic
         title="Temp"
-        value={temperature && temperature.temp}
+        value={heatingInfo && heatingInfo.temp}
         prefix={isHeating && <Icon type="up-circle" theme="twoTone" />}
         valueStyle={isHeating ? { color: blue.primary } : undefined}
         precision={2}
       />
-      <Statistic title="Goal" value={temperature && temperature.goal} />
+      <Statistic title="Goal" value={heatingInfo && heatingInfo.goal} />
       <Statistic
         title="Time Elapsed"
-        value={heatingInfo && heatingInfo.timeElapsed / 1000}
+        value={heatingInfo && MMSS(heatingInfo.timeElapsed)}
         precision={2}
-        suffix="s"
+        suffix="m"
+      />
+      <Statistic
+        title="Time Left"
+        value={heatingInfo && MMSS(heatingInfo.timeRemaining || 0)}
+        precision={2}
+        suffix="m"
       />
     </Card>
   );
--- packages/de1/src/events.ts
+++ packages/de1/src/events.ts
@@ -1,49 +1,38 @@
+import fs from "fs";
+import path from "path";
 import { EventEmitter } from "events";
-import DE1, { De1State } from "./de1";
+import DE1 from "./de1";
 import { StateInfo, State, SubState, Shot } from "./converters";
 
 export interface De1Events {
-  state: De1State;
-  temperature: {
-    goal: number;
-    temp: number;
-  };
   heating: {
     goal: number;
     temp: number;
     timeElapsed: number;
+    timeRemainingExperimental?: number;
     debug?: Record<string, any>;
   };
-  waterlevel: number;
 }
 
-export type De1Listener<N extends keyof De1Events> = (
+export type De1EventName = keyof De1Events;
+
+export type De1Listener<N extends De1EventName> = (
   value: De1Events[N]
 ) => Promise<void> | void;
 
-interface LastState {
+interface LastStateInfo {
   state: State;
-  time: number;
-}
-
-interface LastSubState {
   substate: SubState;
-  time: number;
-}
-
-interface LastShot {
-  shot: Shot;
   time: number;
 }
 
 export default class Events {
   private de1: DE1;
   private emitter = new EventEmitter();
-  private lastState?: LastState;
-  private lastSubState?: LastSubState;
-  private lastShot?: LastShot;
-  private heatingStarted?: number;
-  private heatingHistory: number[] = [];
+  private lastStateInfo?: LastStateInfo;
+  private heatHistory: { time: number; shot: Shot }[] = [];
+  private heatStart?: { time: number; shot: Shot };
+  private heatEnd?: { time: number; shot: Shot };
 
   constructor(de1: DE1) {
     this.de1 = de1;
@@ -56,100 +45,83 @@ export default class Events {
     service.on("shot", this.onShot.bind(this));
   }
 
-  public on<N extends keyof De1Events>(
-    event: N,
-    listener: De1Listener<N>
-  ): void {
+  public unloadListeners(): void {
+    const service = this.de1.getBleService();
+    service.off("stateInfo", this.onStateInfo.bind(this));
+    service.off("shot", this.onShot.bind(this));
+  }
+
+  public on<N extends De1EventName>(event: N, listener: De1Listener<N>): void {
     this.emitter.on(event, listener);
   }
 
-  public off<N extends keyof De1Events>(
-    event: N,
-    listener: De1Listener<N>
-  ): void {
+  public off<N extends De1EventName>(event: N, listener: De1Listener<N>): void {
     this.emitter.off(event, listener);
   }
 
-  public once<N extends keyof De1Events>(
-    event: N,
-    listener: De1Listener<N>
-  ): void {
-    this.emitter.once(event, listener);
-  }
-
-  private onStateInfo(stateInfo: StateInfo): void {
-    const { state, substate } = stateInfo;
-    const { lastState, lastSubState } = this;
-    const time = Date.now();
-
-    if (
-      substate === "heating" &&
-      (!lastSubState || lastSubState.substate !== "heating")
-    ) {
-      this.heatingStarted = Date.now();
+  private async onStateInfo({ state, substate }: StateInfo): Promise<void> {
+    if (!this.isHeating() && substate === "heating") {
+      this.heatHistory = [];
+      this.heatStart = undefined;
     }
-
-    if (!lastState || lastState.state !== state) {
-      this.lastState = { state, time };
+    if (this.isHeating() && substate !== "heating") {
+      const file =
+        "/Users/lbombach/Projekte/DecentEspresso/de1/packages/de1/src/heatinglog.txt";
+      const data = { start: this.heatStart, end: this.heatEnd };
+      await fs.promises.appendFile(file, JSON.stringify(data) + "\n");
     }
-
-    if (!lastSubState || lastSubState.substate !== substate) {
-      this.lastSubState = { substate, time };
-    }
+    this.lastStateInfo = { state, substate, time: Date.now() };
   }
 
   private onShot(shot: Shot): void {
-    this.emitter.emit("temperature", this.getTemperatureParams(shot));
-    if (this.lastSubState && this.lastSubState.substate === "heating") {
-      this.emitter.emit("heating", this.getHeatingParams(shot));
+    if (typeof this.heatStart === "undefined") {
+      this.heatStart = { shot, time: Date.now() };
     }
-    this.updateLastShot(shot);
+    if (this.isHeating()) this.emitHeating(shot);
   }
 
-  private getTemperatureParams(shot: Shot): De1Events["temperature"] {
-    const temp = shot.mixTemp;
-    const goal = shot.setHeadTemp;
-    return { temp, goal };
-  }
-
-  private getHeatingParams(shot: Shot): De1Events["heating"] {
-    const temp = shot.mixTemp;
-    const goal = shot.setHeadTemp;
-    const timeElapsed = Date.now() - (this.heatingStarted || 0);
-    const { heatingHistory } = this;
-    if (!this.lastShot) return { goal, temp, timeElapsed };
-    const timeBetweenShots = (Date.now() - this.lastShot.time) / 1000;
-    const tempBetweenShots = shot.mixTemp - this.lastShot.shot.mixTemp;
-    const tempPerSecond = tempBetweenShots / timeBetweenShots;
-    if (tempPerSecond > 0) {
-      if (heatingHistory.length >= 40) heatingHistory.shift();
-      heatingHistory.push(tempPerSecond);
-    }
-    const tempPerSecondSum = heatingHistory.reduce((a, b) => a + b, 0);
-    const tempPerSecondAvg = tempPerSecondSum / heatingHistory.length;
-    const tempToGo = shot.setHeadTemp - shot.mixTemp;
-    const timeLeft = Math.floor(tempToGo / Math.abs(tempPerSecondAvg));
-    const timeLeftMinutes = (timeLeft / 60).toFixed(2);
-    return {
-      goal,
-      temp,
-      timeElapsed,
-      debug: {
-        timeBetweenShots,
-        tempBetweenShots,
-        tempPerSecond,
-        tempPerSecondSum,
-        tempPerSecondAvg,
-        tempToGo,
-        timeLeft,
-        timeLeftMinutes,
-        heatingHistory
-      }
-    };
-  }
-
-  private updateLastShot(shot: Shot): void {
+  private emitHeating(shot: Shot): void {
     const time = Date.now();
-    this.lastShot = { time, shot };
+    const goal = shot.setHeadTemp;
+    const temp = shot.mixTemp;
+    const timeElapsed = time - this.lastStateInfo!.time;
+
+    this.heatHistory.push({ time, shot });
+    if (this.heatHistory.length > 30) this.heatHistory.shift();
+
+    const startTime = this.heatStart!.time;
+    const startTemp = this.heatStart!.shot.mixTemp;
+
+    const timeDiff = time - startTime;
+    const heatDiff = temp - startTemp;
+    const heatPerSecond = heatDiff / timeDiff;
+    const heatRemaining = goal - temp;
+    const timeRemainingExperimental = Math.floor(heatRemaining / heatPerSecond);
+
+    function MMSS(millis: number) {
+      var minutes = Math.floor(millis / 60000);
+      var seconds = Math.floor((millis % 60000) / 1000);
+      return minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
+    }
+
+    const debug = {
+      startTime,
+      startTemp,
+      timeDiff,
+      heatDiff,
+      heatPerSecond,
+      heatRemaining,
+      timeRemainingExperimental,
+      timeRemainingMinutes: MMSS(timeRemainingExperimental),
+      heatHistoryLength: this.heatHistory.length
+    };
+    const event = { goal, temp, timeElapsed, timeRemainingExperimental, debug };
+    this.emitter.emit("heating", event);
+    this.heatEnd = { time, shot };
+  }
+
+  private isHeating(): boolean {
+    if (!this.lastStateInfo) return false;
+    return this.lastStateInfo.substate === "heating";
   }
 }
