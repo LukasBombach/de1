8f709fa7136175e1460cb4302f87ba823010b0a3
Lukas Bombach <lukas.bombach@googlemail.com>
2019-08-16 23:25:38 +0200

    useevent goodness

--- packages/de1-debugger/src/hooks/useEvent.ts
+++ packages/de1-debugger/src/hooks/useEvent.ts
@@ -1,32 +1,18 @@
-import { useState } from "react";
-import { De1Events, De1Listener } from "de1";
+import { useState, useEffect } from "react";
+import { De1Events } from "de1";
 import de1 from "../lib/de1";
 
-export default function useEvent<N extends keyof De1Events>(
-  name: N
-): De1Events[N] | undefined {
-  const [isListening, setIsListening] = useState(false);
-  const [value, setValue] = useState<De1Events[N] | undefined>(undefined);
+export default function useEvent<E extends keyof De1Events>(
+  event: E
+): De1Events[E] | undefined {
+  const [value, setValue] = useState<De1Events[E] | undefined>();
 
-  const listener: De1Listener<N> = value => setValue(value);
-  const [[persistedListener]] = useState([listener]);
-
-  const start = () => {
-    if (isListening) return;
-    de1.on(name, persistedListener);
-    return setIsListening(true);
-  };
-
-  const stop = () => {
-    if (!isListening) return;
-    de1.off(name, persistedListener);
-    return setIsListening(false);
-  };
-
-  de1.on("connected", () => start());
-  de1.on("disconnected", () => stop());
-
-  if (de1.isConnected()) start();
+  useEffect(() => {
+    de1.on(event, setValue);
+    return () => {
+      de1.off(event, setValue);
+    };
+  });
 
   return value;
 }
