3b7a8449ebdbd7aeb16fcea5c69baef848e7448a
Lukas Bombach <lukas.bombach@googlemail.com>
2019-08-16 23:21:42 +0200

    usenotify goodness

--- packages/de1-debugger/src/hooks/useNotify.ts
+++ packages/de1-debugger/src/hooks/useNotify.ts
@@ -1,19 +1,18 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Feature, Value } from "de1";
 import de1 from "../lib/de1";
 
 export default function useNotify<F extends Feature>(
   name: F
 ): Value<F> | undefined {
-  const [value, setValue] = useState<Value<F> | undefined>();
+  const [value, setValue] = useState<Value<F>>();
 
-  const start = () => de1.getBleService().on(name, setValue);
-  const stop = () => de1.getBleService().off(name, setValue);
-
-  de1.on("connected", () => start());
-  de1.on("disconnected", () => stop());
-
-  if (de1.isConnected()) start();
+  useEffect(() => {
+    de1.getBleService().on(name, setValue);
+    return () => {
+      de1.getBleService().off(name, setValue);
+    };
+  });
 
   return value;
 }
