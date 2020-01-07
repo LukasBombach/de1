825ede058cf0f5d9c8beb4f4699cc09c9cd2b35d
Lukas Bombach <lukas.bombach@googlemail.com>
2020-01-07 06:44:21 +0100

    working on state implementation

--- packages/de1/src/state.ts
+++ packages/de1/src/state.ts
@@ -1,52 +1,18 @@
+type DE1State = string;
+
 export default class State {
+  public async start(state: DE1State): Promise<void> {
+    await this.write(state);
+  }
+
+  public async stop(state: DE1State): Promise<void> {
+    const currentState = await this.read();
+    if (currentState === state) await this.write("idle");
+  }
+
   async stopEverything(): Promise<void> {
-    const currentState = await this.read("state");
-    if (currentState !== "sleep") await this.write("state", "idle");
-  }
-
-  async startEspresso(): Promise<void> {
-    await this.write("state", "espresso");
-  }
-
-  async stopEspresso(): Promise<void> {
-    const currentState = await this.read("state");
-    if (currentState === "espresso") await this.write("state", "idle");
-  }
-
-  async startSteam(): Promise<void> {
-    await this.write("state", "steam");
-  }
-
-  async stopSteam(): Promise<void> {
-    const currentState = await this.read("state");
-    if (currentState === "steam") await this.write("state", "idle");
-  }
-
-  async startHotWater(): Promise<void> {
-    await this.write("state", "hotWater");
-  }
-
-  async stopHotWater(): Promise<void> {
-    const currentState = await this.read("state");
-    if (currentState === "hotWater") await this.write("state", "idle");
-  }
-
-  async startFlushing(): Promise<void> {
-    await this.write("state", "hotWaterRinse");
-  }
-
-  async stopFlushing(): Promise<void> {
-    const currentState = await this.read("state");
-    if (currentState === "hotWaterRinse") await this.write("state", "idle");
-  }
-
-  async startDescaling(): Promise<void> {
-    await this.write("state", "descale");
-  }
-
-  async stopDescaling(): Promise<void> {
-    const currentState = await this.read("state");
-    if (currentState === "descale") await this.write("state", "idle");
+    const currentState = await this.read();
+    if (currentState !== "sleep") await this.write("idle");
   }
 
   async getState(): Promise<string> {
@@ -62,11 +28,11 @@ export default class State {
     return "idle";
   }
 
-  private async read(name: string) {
-    return await this.getBleService().read(name);
+  public async read() {
+    return await this.getBleService().read("state");
   }
 
-  private async write(name: string, value: string) {
-    await this.getBleService().write(name, value);
+  public async write(value: string) {
+    await this.getBleService().write(value);
   }
 }
