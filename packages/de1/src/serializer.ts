ba720f73113a5b0cf27eb7260adb0930cc267f27
Lukas Bombach <lukas.bombach@googlemail.com>
2020-01-18 18:34:00 +0100

    refactored serializer

--- packages/de1/src/serializer.ts
+++ packages/de1/src/serializer.ts
@@ -1,82 +1,40 @@
-type BufferType = "Uint8" | "Uint16" | "Uint32";
-type BufferLengths = { [bufferType in BufferType]: number };
+export default class Serializer {
+  public buffer = Buffer.alloc(0);
 
-// https://medium.com/dailyjs/typescript-create-a-condition-based-subset-types-9d902cea5b8c
-// type DataViewFunction = Pick<
-//   DataView,
-//   {
-//     [Key in keyof DataView]: DataView[Key] extends Function ? Key : never
-//   }[keyof DataView]
-// >;
-
-type Value = {
-  type: BufferType;
-  value: number;
-  littleEndian?: boolean;
-};
-
-export default class Serializer<T> {
-  private static bufferLengths: BufferLengths = {
-    Uint8: 1,
-    Uint16: 2,
-    Uint32: 4,
-  };
-
-  private values: Value[] = [];
-
-  char(value: number) {
-    this.add("Uint8", value);
+  char(value: number): this {
+    this.addUintBuffer(1, value);
     return this;
   }
 
-  short(value: number) {
-    this.add("Uint16", value, false);
+  short(value: number): this {
+    this.addUintBuffer(2, value);
     return this;
   }
 
-  int(value: number) {
-    this.add("Uint16", value, true);
+  int(value: number): this {
+    this.addUintBuffer(4, value);
     return this;
   }
 
-  sha(value: string) {
-    this.add("Uint16", parseInt(value, 16), true);
+  intSigned(value: number): this {
+    this.addIntBuffer(4, value);
     return this;
   }
 
-  dataView(): DataView {
-    const length = this.getBufferLength();
-    const buffer = new ArrayBuffer(length);
-    const dataView = new DataView(buffer);
-    this.setValues(dataView);
-    return dataView;
+  sha(value: string): this {
+    this.addIntBuffer(4, parseInt(value, 16));
+    return this;
   }
 
-  buffer(): Buffer {
-    return Buffer.from(this.dataView().buffer);
+  private addUintBuffer(byteLength: number, value: number): void {
+    const buffer = Buffer.alloc(byteLength);
+    buffer.writeUIntBE(value, 0, byteLength);
+    this.buffer = Buffer.concat([this.buffer, buffer]);
   }
 
-  // todo bad typecasting
-  private setValues(dataView: DataView, offset = 0): void {
-    this.values.forEach(({ type, value, littleEndian }) => {
-      const method = `set${type}` as keyof DataView;
-      (dataView[method] as Function)(offset, value, littleEndian);
-      offset += this.typeLength(type);
-    });
-  }
-
-  private add(type: BufferType, value: number, littleEndian?: boolean): void {
-    if (typeof value === "undefined") return;
-    this.values.push({ type, value, littleEndian });
-  }
-
-  private getBufferLength(): number {
-    return this.values
-      .map(({ type }) => this.typeLength(type))
-      .reduce((sum, length) => sum + length, 0);
-  }
-
-  private typeLength(type: BufferType): number {
-    return Serializer.bufferLengths[type];
+  private addIntBuffer(byteLength: number, value: number): void {
+    const buffer = Buffer.alloc(byteLength);
+    buffer.writeIntBE(value, 0, byteLength);
+    this.buffer = Buffer.concat([this.buffer, buffer]);
   }
 }
