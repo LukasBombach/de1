18a633341958c0b7f3a45df12a8b088d33b4113e
Oscar Bolmsten <oscar@bolmsten.se>
2020-01-28 08:46:44 +0100

    Improve test coverage (#33)
    * Improve test coverage
    
    * turnOn sets the machine's state to idle if state is sleep

--- packages/de1/tests/de1/connection.test.ts
+++ packages/de1/tests/de1/connection.test.ts
@@ -1,5 +1,6 @@
 import Sblendid, { Peripheral } from "@sblendid/sblendid";
 import DE1 from "../../src/de1";
+import Machine from "../../src/machine";
 
 describe("de1 connection", () => {
   it("connects to the DE1 machine", async () => {
@@ -18,5 +19,26 @@ describe("de1 connection", () => {
     await expect(de1.disconnect()).resolves.toBeUndefined();
     expect(spy).toHaveBeenCalledWith();
     spy.mockRestore();
+  });
+
+  it("doesn't try to connect when already connected", async () => {
+    const isConnectedSpy = jest
+      .spyOn(Machine.prototype, "isConnected")
+      .mockImplementation(() => true);
+    const connectSpy = jest.spyOn(Sblendid, "connect");
+    const de1 = new DE1();
+    await expect(de1.connect()).resolves.toBeUndefined();
+    expect(connectSpy).not.toHaveBeenCalled();
+    expect(isConnectedSpy).toHaveBeenCalled();
+    connectSpy.mockRestore();
+    isConnectedSpy.mockRestore();
+  });
+
+  it("doesn't try to disconnect when not connected", async () => {
+    const disconnectSpy = jest.spyOn(Peripheral.prototype, "disconnect");
+    const de1 = new DE1();
+    await de1.disconnect();
+    expect(disconnectSpy).not.toHaveBeenCalled();
+    disconnectSpy.mockRestore();
   });
 });
